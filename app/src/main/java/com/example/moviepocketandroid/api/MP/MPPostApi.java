ce8f15ea84a8d91c99fb00af0d5fcd9bdc6acdd0
Anton <poznyak969@gmail.com>
2023-12-27 12:59:04 +0100

    add request post review

--- app/src/main/java/com/example/moviepocketandroid/api/MP/MPPostApi.java
+++ app/src/main/java/com/example/moviepocketandroid/api/MP/MPPostApi.java
@@ -1,15 +1,26 @@
 package com.example.moviepocketandroid.api.MP;
 
+import com.example.moviepocketandroid.api.models.list.MovieList;
+import com.example.moviepocketandroid.api.models.post.Post;
 import com.example.moviepocketandroid.util.LocalDateAdapter;
 import com.example.moviepocketandroid.util.LocalDateTimeAdapter;
 import com.example.moviepocketandroid.util.Utils;
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
 
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.IOException;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
 
 import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
 
 public class MPPostApi {
 
@@ -20,5 +31,75 @@ public class MPPostApi {
             .registerTypeAdapter(LocalDateTime.class, new LocalDateTimeAdapter())
             .create();
 
+    public static Post getPostById(int idPost) {
+        String url = baseUrl + "/post/get?idPost=" + idPost;
+
+        Request request = new Request.Builder()
+                .url(url)
+                .get()
+                .build();
+
+        try {
+            Response response = client.newCall(request).execute();
+            if (response.isSuccessful()) {
+                assert response.body() != null;
+                String responseString = response.body().string();
+                return gson.fromJson(responseString, Post.class);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+    public static List<MovieList> getAllPostExistIdMovie(int idPost) {
+        List<MovieList> lists = new ArrayList<>();
+        String url = baseUrl + "/post/movie?idMovie=" + idPost;
+
+        Request request = new Request.Builder()
+                .url(url)
+                .get()
+                .build();
+
+        try {
+            Response response = client.newCall(request).execute();
+
+            if (response.isSuccessful()) {
+                String responseBody = response.body().string();
+                JSONArray reviewArray = new JSONArray(responseBody);
+                for (int i = 0; i < reviewArray.length(); i++) {
+                    JSONObject reviewObject = reviewArray.getJSONObject(i);
+                    MovieList movieList = gson.fromJson(reviewObject.toString(), MovieList.class);
+                    if (movieList != null) {
+                        lists.add(movieList);
+                    }
+                }
+            }
+        } catch (IOException | JSONException e) {
+            e.printStackTrace();
+        }
+        return lists;
+    }
+
+    public static Boolean getAuthorship(int idPost) {
+        String url = baseUrl + "/post/authorship?idPost=" + idPost;
+
+        Request request = new Request.Builder()
+                .url(url)
+                .get()
+                .addHeader("Cookie", MPAuthenticationApi.getCookies())
+                .build();
+
+        try {
+            Response response = client.newCall(request).execute();
+
+            if (response.isSuccessful()) {
+                return !response.body().string().equals("false");
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return false;
+    }
 
 }
--- app/src/main/java/com/example/moviepocketandroid/ui/post/PostFragment.java
+++ app/src/main/java/com/example/moviepocketandroid/ui/post/PostFragment.java
@@ -1,6 +1,8 @@
 package com.example.moviepocketandroid.ui.post;
 
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Looper;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -8,13 +10,24 @@ import android.view.ViewGroup;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
-import androidx.lifecycle.ViewModelProvider;
+import androidx.fragment.app.FragmentPagerAdapter;
+import androidx.viewpager.widget.ViewPager;
 
 import com.example.moviepocketandroid.R;
+import com.example.moviepocketandroid.adapter.ListNavBarAdapter;
+import com.example.moviepocketandroid.api.MP.MPListApi;
+import com.example.moviepocketandroid.ui.list.edit.ListEditFragment;
+import com.example.moviepocketandroid.ui.list.movie.ListMovieFragment;
+import com.example.moviepocketandroid.ui.review.all.AllReviewFragment;
+import com.google.android.material.tabs.TabLayout;
 
 public class PostFragment extends Fragment {
 
     private PostViewModel mViewModel;
+    private TabLayout tabLayout;
+    private ViewPager viewPager;
+    private int idPost;
+
 
     public static PostFragment newInstance() {
         return new PostFragment();
@@ -27,11 +40,41 @@ public class PostFragment extends Fragment {
     }
 
     @Override
-    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
-        super.onActivityCreated(savedInstanceState);
-        mViewModel = new ViewModelProvider(this).get(PostViewModel.class);
-        // TODO: Use the ViewModel
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        Bundle args = getArguments();
+        if (args != null) {
+            idPost = args.getInt("idPost");
+
+            tabLayout = view.findViewById(R.id.tabLayout);
+            viewPager = view.findViewById(R.id.viewPager);
+
+            tabLayout.setupWithViewPager(viewPager);
+
+            ListNavBarAdapter listNavBarAdapter = new ListNavBarAdapter(getChildFragmentManager(),
+                    FragmentPagerAdapter.BEHAVIOR_RESUME_ONLY_CURRENT_FRAGMENT);
+
+            new Thread(new Runnable() {
+                @Override
+                public void run() {
+                    Boolean authorship = MPListApi.getAuthorship(idPost);
+
+                    new Handler(Looper.getMainLooper()).post(new Runnable() {
+                        @Override
+                        public void run() {
+                            listNavBarAdapter.addFragmentAddTitle(new ListMovieFragment(idPost), "Post");
+                            listNavBarAdapter.addFragmentAddTitle(new AllReviewFragment(idPost), "Reviews");
+                            if (authorship)
+                                listNavBarAdapter.addFragmentAddTitle(new ListEditFragment(), "Edit");
+                            viewPager.setAdapter(listNavBarAdapter);
+                        }
+                    });
+
+                }
+            }).start();
+
+
+        }
     }
-
-
 }
