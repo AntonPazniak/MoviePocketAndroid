e70fc5e05b2310996eea0b3b4307b235507674ab
Anton <poznyak969@gmail.com>
2023-12-27 14:20:42 +0100

    minor fix

--- app/src/main/java/com/example/moviepocketandroid/api/models/post/Post.java
+++ app/src/main/java/com/example/moviepocketandroid/api/models/post/Post.java
@@ -2,7 +2,7 @@ package com.example.moviepocketandroid.api.models.post;
 
 import com.example.moviepocketandroid.api.models.user.User;
 
-import java.util.Date;
+import java.time.LocalDateTime;
 
 import lombok.AllArgsConstructor;
 import lombok.Getter;
@@ -19,19 +19,89 @@ public class Post {
     private String content;
     private int[] likeOrDis;
     private User user;
-    private Date create;
-    private Date update;
+    private LocalDateTime create;
+    private LocalDateTime update;
     private Long idMovie;
     private Long idPerson;
     private Long idList;
 
-    public Post(Long id, String title, String content, int[] likeOrDis, User user, Date create, Date update) {
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
         this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
         this.title = title;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
         this.content = content;
+    }
+
+    public int[] getLikeOrDis() {
+        return likeOrDis;
+    }
+
+    public void setLikeOrDis(int[] likeOrDis) {
         this.likeOrDis = likeOrDis;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
         this.user = user;
+    }
+
+    public LocalDateTime getCreate() {
+        return create;
+    }
+
+    public void setCreate(LocalDateTime create) {
         this.create = create;
+    }
+
+    public LocalDateTime getUpdate() {
+        return update;
+    }
+
+    public void setUpdate(LocalDateTime update) {
         this.update = update;
+    }
+
+    public Long getIdMovie() {
+        return idMovie;
+    }
+
+    public void setIdMovie(Long idMovie) {
+        this.idMovie = idMovie;
+    }
+
+    public Long getIdPerson() {
+        return idPerson;
+    }
+
+    public void setIdPerson(Long idPerson) {
+        this.idPerson = idPerson;
+    }
+
+    public Long getIdList() {
+        return idList;
+    }
+
+    public void setIdList(Long idList) {
+        this.idList = idList;
     }
 }
