10b2792ec9667484abf9389ed53b7be5b0ca4c76
Anton <poznyak969@gmail.com>
2024-01-02 22:48:47 +0100

    add start activity

---
+++ app/src/main/java/com/example/moviepocketandroid/adapter/ImagePagerAdapter.java
@@ -0,0 +1,52 @@
+package com.example.moviepocketandroid.adapter;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.example.moviepocketandroid.R;
+
+import java.util.List;
+
+public class ImagePagerAdapter extends RecyclerView.Adapter<ImagePagerAdapter.ImageViewHolder> {
+    private List<Integer> images;
+    private LayoutInflater layoutInflater;
+    private Context context;
+
+    public ImagePagerAdapter(Context context, List<Integer> images) {
+        this.context = context;
+        this.images = images;
+        this.layoutInflater = LayoutInflater.from(context);
+    }
+
+    @NonNull
+    @Override
+    public ImageViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = layoutInflater.inflate(R.layout.item_image, parent, false);
+        return new ImageViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ImageViewHolder holder, int position) {
+        holder.imageView.setImageResource(images.get(position));
+    }
+
+    @Override
+    public int getItemCount() {
+        return images.size();
+    }
+
+    static class ImageViewHolder extends RecyclerView.ViewHolder {
+        ImageView imageView;
+
+        ImageViewHolder(View itemView) {
+            super(itemView);
+            imageView = itemView.findViewById(R.id.imageView);
+        }
+    }
+}
