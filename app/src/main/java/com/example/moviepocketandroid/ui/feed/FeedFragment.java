ff8ca74f5a9d814a767f683885a7a295acfdde89
Anton <poznyak969@gmail.com>
2024-01-06 01:24:06 +0100

    add feed fragment

---
+++ app/src/main/java/com/example/moviepocketandroid/ui/feed/FeedFragment.java
@@ -0,0 +1,60 @@
+package com.example.moviepocketandroid.ui.feed;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentPagerAdapter;
+import androidx.lifecycle.ViewModelProvider;
+import androidx.viewpager.widget.ViewPager;
+
+import com.example.moviepocketandroid.R;
+import com.example.moviepocketandroid.adapter.ListNavBarAdapter;
+import com.example.moviepocketandroid.ui.home.HomeFragment;
+import com.google.android.material.tabs.TabLayout;
+
+public class FeedFragment extends Fragment {
+
+    private FeedViewModel mViewModel;
+    private TabLayout tabLayout;
+    private ViewPager viewPager;
+    private ListNavBarAdapter listNavBarAdapter;
+
+    public static FeedFragment newInstance() {
+        return new FeedFragment();
+    }
+
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container,
+                             @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_feed, container, false);
+    }
+
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+        mViewModel = new ViewModelProvider(this).get(FeedViewModel.class);
+        // TODO: Use the ViewModel
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        tabLayout = view.findViewById(R.id.tabLayout);
+        viewPager = view.findViewById(R.id.viewPager);
+
+        tabLayout.setupWithViewPager(viewPager);
+
+        listNavBarAdapter = new ListNavBarAdapter(getChildFragmentManager(),
+                FragmentPagerAdapter.BEHAVIOR_RESUME_ONLY_CURRENT_FRAGMENT);
+
+        listNavBarAdapter.addFragmentAddTitle(new HomeFragment(), "Lists");
+        listNavBarAdapter.addFragmentAddTitle(new HomeFragment(), "Posts");
+        viewPager.setAdapter(listNavBarAdapter);
+    }
+}
---
+++ app/src/main/java/com/example/moviepocketandroid/ui/feed/FeedViewModel.java
@@ -0,0 +1,7 @@
+package com.example.moviepocketandroid.ui.feed;
+
+import androidx.lifecycle.ViewModel;
+
+public class FeedViewModel extends ViewModel {
+    // TODO: Implement the ViewModel
+}
