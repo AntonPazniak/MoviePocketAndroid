81eed378568e7945971c53599c09cf9e6ca6df1a
Anton <poznyak969@gmail.com>
2024-01-06 18:16:01 +0100

    add request for list and post last/top

--- app/src/main/java/com/example/moviepocketandroid/api/MP/MPListApi.java
+++ app/src/main/java/com/example/moviepocketandroid/api/MP/MPListApi.java
@@ -315,4 +315,64 @@ public class MPListApi {
         }
         return false;
     }
+
+    public static List<MovieList> getLastLists() {
+        List<MovieList> lists = new ArrayList<>();
+        String url = baseUrl + "/movies/list/get/last";
+
+        Request request = new Request.Builder()
+                .url(url)
+                .get()
+                .build();
+
+        try {
+            Response response = client.newCall(request).execute();
+
+            if (response.isSuccessful()) {
+                String responseBody = response.body().string();
+                JSONArray reviewArray = new JSONArray(responseBody);
+                for (int i = 0; i < reviewArray.length(); i++) {
+                    JSONObject reviewObject = reviewArray.getJSONObject(i);
+                    MovieList movieList = gson.fromJson(reviewObject.toString(), MovieList.class);
+                    if (movieList != null) {
+                        lists.add(movieList);
+                    }
+                }
+            }
+        } catch (IOException | JSONException e) {
+            e.printStackTrace();
+        }
+        return lists;
+    }
+
+    public static List<MovieList> getTopLists() {
+        List<MovieList> lists = new ArrayList<>();
+        String url = baseUrl + "/movies/list/get/top";
+
+        Request request = new Request.Builder()
+                .url(url)
+                .get()
+                .build();
+
+        try {
+            Response response = client.newCall(request).execute();
+
+            if (response.isSuccessful()) {
+                String responseBody = response.body().string();
+                JSONArray reviewArray = new JSONArray(responseBody);
+                for (int i = 0; i < reviewArray.length(); i++) {
+                    JSONObject reviewObject = reviewArray.getJSONObject(i);
+                    MovieList movieList = gson.fromJson(reviewObject.toString(), MovieList.class);
+                    if (movieList != null) {
+                        lists.add(movieList);
+                    }
+                }
+            }
+        } catch (IOException | JSONException e) {
+            e.printStackTrace();
+        }
+        return lists;
+    }
+
+
 }
--- app/src/main/java/com/example/moviepocketandroid/api/MP/MPPostApi.java
+++ app/src/main/java/com/example/moviepocketandroid/api/MP/MPPostApi.java
@@ -265,5 +265,63 @@ public class MPPostApi {
         return posts;
     }
 
+    public static List<Post> getLastPosts() {
+        List<Post> posts = new ArrayList<>();
+        String url = baseUrl + "/post/get/last";
+
+        Request request = new Request.Builder()
+                .url(url)
+                .get()
+                .build();
+
+        try {
+            Response response = client.newCall(request).execute();
+
+            if (response.isSuccessful()) {
+                String responseBody = response.body().string();
+                JSONArray reviewArray = new JSONArray(responseBody);
+                for (int i = 0; i < reviewArray.length(); i++) {
+                    JSONObject reviewObject = reviewArray.getJSONObject(i);
+                    Post post = gson.fromJson(reviewObject.toString(), Post.class);
+                    if (post != null) {
+                        posts.add(post);
+                    }
+                }
+            }
+        } catch (IOException | JSONException e) {
+            e.printStackTrace();
+        }
+        return posts;
+    }
+
+    public static List<Post> getTopPosts() {
+        List<Post> posts = new ArrayList<>();
+        String url = baseUrl + "/post/get/top";
+
+        Request request = new Request.Builder()
+                .url(url)
+                .get()
+                .build();
+
+        try {
+            Response response = client.newCall(request).execute();
+
+            if (response.isSuccessful()) {
+                String responseBody = response.body().string();
+                JSONArray reviewArray = new JSONArray(responseBody);
+                for (int i = 0; i < reviewArray.length(); i++) {
+                    JSONObject reviewObject = reviewArray.getJSONObject(i);
+                    Post post = gson.fromJson(reviewObject.toString(), Post.class);
+                    if (post != null) {
+                        posts.add(post);
+                    }
+                }
+            }
+        } catch (IOException | JSONException e) {
+            e.printStackTrace();
+        }
+        return posts;
+    }
+
 
 }
