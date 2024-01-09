15d7f3e6fac3b2dc3dbf7347ba9d07612a57ac99
Anton <poznyak969@gmail.com>
2024-01-09 14:13:35 +0100

    add setNewAvatar

--- app/src/main/java/com/example/moviepocketandroid/api/MP/MPUserApi.java
+++ app/src/main/java/com/example/moviepocketandroid/api/MP/MPUserApi.java
@@ -1,5 +1,8 @@
 package com.example.moviepocketandroid.api.MP;
 
+import android.content.Context;
+import android.net.Uri;
+
 import com.example.moviepocketandroid.api.models.user.User;
 import com.example.moviepocketandroid.api.models.user.UserPage;
 import com.example.moviepocketandroid.util.LocalDateAdapter;
@@ -8,15 +11,20 @@ import com.example.moviepocketandroid.util.Utils;
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
 
+import java.io.ByteArrayOutputStream;
 import java.io.IOException;
+import java.io.InputStream;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
 
 import okhttp3.FormBody;
+import okhttp3.MediaType;
+import okhttp3.MultipartBody;
 import okhttp3.OkHttpClient;
 import okhttp3.Request;
 import okhttp3.RequestBody;
 import okhttp3.Response;
+
 
 public class MPUserApi {
 
@@ -145,6 +153,49 @@ public class MPUserApi {
             e.printStackTrace();
         }
         return null;
+    }
+
+    public static Boolean setNewAvatar(Context context, Uri selectedImage) {
+        String url = baseUrl + "/user/edit/newAvatar";
+
+        try {
+            // Получение потока данных из Uri
+            InputStream inputStream = context.getContentResolver().openInputStream(selectedImage);
+
+            // Создание объекта запроса для изображения
+            RequestBody requestBody = new MultipartBody.Builder()
+                    .setType(MultipartBody.FORM)
+                    .addFormDataPart("file", "avatar.jpg", RequestBody.create(MediaType.parse("image/*"), inputStreamToByteArray(inputStream)))
+                    .build();
+
+            Request request = new Request.Builder()
+                    .url(url)
+                    .post(requestBody)
+                    .addHeader("Cookie", MPAuthenticationApi.getCookies())
+                    .build();
+
+            Response response = client.newCall(request).execute();
+
+            if (response.isSuccessful()) {
+                return true;
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return false;
+    }
+
+    private static byte[] inputStreamToByteArray(InputStream inputStream) throws IOException {
+        ByteArrayOutputStream byteBuffer = new ByteArrayOutputStream();
+        int bufferSize = 8192;
+        byte[] buffer = new byte[bufferSize];
+
+        int len;
+        while ((len = inputStream.read(buffer)) != -1) {
+            byteBuffer.write(buffer, 0, len);
+        }
+
+        return byteBuffer.toByteArray();
     }
 
 
